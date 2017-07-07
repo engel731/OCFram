5223b9bcee0130e4e0e986d36e9e50d990558dcf
engel731 <tanguy731@hotmail.fr>
2017-07-07 10:10:30 +0200

    Ajout d'un champ

---
+++ lib/Form/Field/FileField.php
@@ -0,0 +1,19 @@
+<?php
+namespace OCFram\Form\Field;
+
+use \OCFram\Field;
+
+class FileField extends Field
+{
+  public function buildWidget()
+  {
+    $widget = '';
+    
+    if (!empty($this->errorMessage))
+    {
+      $widget .= $this->errorMessage.'<br />';
+    }
+    
+    return $widget .= '<label>'.$this->label.'</label><input type="file" name="'.$this->name.'" />';
+  }
+}
--- lib/PDOFactory.php
+++ lib/PDOFactory.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
   public static function getMysqlConnexion()
   {
-    $db = new \PDO('mysql:host=localhost;dbname=news', 'root', '');
+    $db = new \PDO('mysql:host=localhost;dbname=jeanforteroche', 'jeanForteroche', 'secret');
     $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     
     return $db;
